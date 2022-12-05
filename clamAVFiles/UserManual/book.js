c646d5d08b6e2c26d03e8089fda6eba2f1fec250
Daisy Aguilar <daisy.aguilar036@gmail.com>
2022-12-05 14:30:50 -0500

    updating clamAV folder

--- clamAVFiles/UserManual/book.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:928d1c9d6472142651bc1b9ad6d1c68cc00ebb73fe1468fa1234f4e8e6693a82
-size 24061
--- clamAVFiles/UserManual/searchindex.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5cf9fc2fe94c3b819d74d6cb78f76b8890183995ebb51fb7dcc6e4ac0ffa3bc1
-size 2955337
